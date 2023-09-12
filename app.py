852feb7698a6316d4178202d3dd84eb59e2be093
soohian <shonendumm@gmail.com>
2023-09-12 23:28:40 +0800

    working json sending from client

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, request, url_for, redirect
-
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        # Do something
-        form_data = request.form
-        print(form_data)
-        return render_template('index.html',form_data=form_data)
-    else:
-        return render_template('index.html',form_data=None)
-        
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-if __name__ == "__main__":
+@app.route('/process_json', methods=['POST'])
+def process_json():
     try:
-        app.run(debug=True)
+        # Get JSON data from the client
+        data = request.get_json()
+        print(data)
+        # Process the JSON data (e.g., perform some calculations)
+        result = process_data(data)
+
+        # Create a response JSON
+        response = {'message': 'JSON data processed successfully', 'result': result}
+
+        return jsonify(response), 200
     except Exception as e:
-        print(e)
+        error_message = str(e)
+        return jsonify({'error': error_message}), 400
+
+def process_data(data):
+    # Replace this with your own data processing logic
+    # For demonstration, we'll simply return the input data as is
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True)
